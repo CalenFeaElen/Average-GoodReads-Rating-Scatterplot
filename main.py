68a285ea97082ed1849b10dfd89fd80503488322
Chelsea_D <calenfeaelen@gmail.com>
2020-04-14 03:13:10 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+with open("books.csv","r") as datafile:
+    data = pd.read_csv(datafile,delimiter=",")
+
+#print(data["language_code"])
+
+sns.scatterplot(x="ratings_count", y="average_rating", hue="average_rating", data=data)
+
+plt.title("Average GoodRead Rating and Number of Ratings")
+
+plt.savefig("avg_rtg_rtg_count.png")
